95a4a603622f5cc5dabe6339b176a44dd7aa5d9b
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-25 14:28:08 +0530

    Initial Commit

---
+++ InstaUI/InstaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaUIApp.swift
+//  InstaUI
+//
+//  Created by shehan karunarathna on 2022-02-25.
+//
+
+import SwiftUI
+
+@main
+struct InstaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
